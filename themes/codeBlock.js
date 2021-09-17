a778ed488869001d8337d4763b5aa34515994ef2
zglgithubx <786945363@qq.com>
2021-09-17 17:05:22 +0800

    update

--- themes/codeBlock.js
+++ themes/codeBlock.js
@@ -4,13 +4,7 @@
 <script src="//cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js">
 </script>
 <script>
+ alert("fadf");
     hljs.initHighlightingOnLoad();
 </script>
 <% } %>
-
-<link href="https://cdn.bootcdn.net/ajax/libs/highlight.js/11.2.0/styles/ir-black.min.css" rel="stylesheet">
-<script src="//cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js">
-<script>
-    alert("fadf");
-    hljs.initHighlightingOnLoad();
-</script>
