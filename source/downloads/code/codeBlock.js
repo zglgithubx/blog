aac08f1950e501ef4e30d8aa89d7fca4985a3c12
zglgithubx <786945363@qq.com>
2021-09-18 11:36:51 +0800

    update

--- source/downloads/code/codeBlock.js
+++
@@ -1,6 +0,0 @@
-<link href="//cdn.bootcdn.net/ajax/libs/highlight.js/11.2.0/styles/ir-black.min.css" rel="stylesheet">
-<script src="//cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js">
-</script>
-<script>
-    hljs.initHighlightingOnLoad();
-</script>
