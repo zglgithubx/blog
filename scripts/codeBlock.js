ae663199f81aeb501573c2645d7530e2fabda06a
zglgithubx <786945363@qq.com>
2021-09-18 11:00:51 +0800

    update

--- scripts/codeBlock.js
+++ scripts/codeBlock.js
@@ -1,9 +1,3 @@
-<% if (1) { %>
-<!-- Highlight.js -->
-<link href="https://cdn.bootcdn.net/ajax/libs/highlight.js/11.2.0/styles/ir-black.min.css" rel="stylesheet">
-<script src="https://cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js">
-</script>
-<script>
-    hljs.initHighlightingOnLoad();
-</script>
-<% } %>
+document.write("<link rel=\"stylesheet\" href=\"https://cdn.bootcdn.net/ajax/libs/highlight.js/11.2.0/styles/ir-black.min.css\"/>");
+document.write("<script type=\"text/javascript\" src=\"https://cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js\"></script>");
+hljs.initHighlightingOnLoad();
