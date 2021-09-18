474e020246c6dae459bba0686f1e56199f73a827
zglgithubx <786945363@qq.com>
2021-09-18 10:47:51 +0800

    update

--- scripts/codeBlock.js
+++ scripts/codeBlock.js
@@ -1,6 +1,6 @@
-<link href="//cdn.bootcdn.net/ajax/libs/highlight.js/11.2.0/styles/ir-black.min.css" rel="stylesheet">
-<script src="//cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js">
-</script>
+<link href="//cdn.bootcdn.net/ajax/libs/highlight.js/11.2.0/styles/ir-black.min.css" rel="stylesheet"/>
+<script src="//cdn.bootcss.com/highlight.js/9.2.0/highlight.min.js"/>
+
 <script>
     hljs.initHighlightingOnLoad();
 </script>
